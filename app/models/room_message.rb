a33831fb548e7f62c1246de43c5a87c5ebe7d441
hunaba <abadeer.hunson@yandex.ru>
2019-12-10 20:33:36 +0100

    chat qui fonctionne ii

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,4 +1,9 @@
 class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
   belongs_to :punchliner
+
+  def as_json(options)
+  super(options).merge(punchliner_avatar_url: punchliner.gravatar_url)
+end
+
 end
