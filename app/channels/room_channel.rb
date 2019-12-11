3adedd03cfd46ea04bf6f778afdc90c4870132f6
hunaba <abadeer.hunson@yandex.ru>
2019-12-11 14:30:53 +0100

    commit pr chat pr tester sur hero

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,9 +1,13 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     room = Room.find params[:room]
-    stream_for room
+   # stream_for room
 
     # or
-    # stream_from "room_#{params[:room]}"
+     stream_from "room_#{params[:room]}"
   end
+
+  #def speak(data)
+   # ActionCable.server.broadcast "room_channel", message: data['message']
+  #end
 end
